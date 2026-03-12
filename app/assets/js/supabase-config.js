d3661d7e9de3d486caadf7f6f87e86043ec121f8
centroOperacional <botplanilhafinanceira@gmail.com>
2026-03-11 23:45:25 -0300

    Update supabase-config.js

--- app/assets/js/supabase-config.js
+++ app/assets/js/supabase-config.js
@@ -2,9 +2,9 @@
 // CONFIGURAÇÃO CENTRAL DO SUPABASE
 // ============================================================
 // URL do projeto
-const SUPABASE_URL = "https://@fthrnmzjskfvuiseoeln.supabase.co";
+const SUPABASE_URL = "https://dseogmdfovdcvpnhqrvv.supabase.co";
 // Chave pública (anon/public)
-const SUPABASE_KEY = "@eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0aHJubXpqc2tmdnVpc2VvZWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NDQ2OTUsImV4cCI6MjA4ODUyMDY5NX0.GGL0pCJgYMqYuNSVXaNz8qLnFGhbFX--fBussEpN9kY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzZW9nbWRmb3ZkY3ZwbmhxcnZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyODE2MTIsImV4cCI6MjA4ODg1NzYxMn0.aAtYxm_o624z6BPam9M4UA-Hz5QpMqmA203tj1FexvM";
 // Instância única do cliente
 let supabaseClient = null;
 // ============================================================
